1f0bd37967d2766616f646567751505bae37b6a9
knazim667 <nazamzahoor@yahoo.com>
2020-12-30 23:30:41 -0500

    add routes cors bodyparser mongoose connection

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
+const cors = require("cors");
 const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const routes = require("./routes/routing");
 
 const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use("/", routes);
 
 const port = process.env.PORT || 4000;
+
+mongoose
+  .connect("mongodb://localhost:27017/cargoExpress", {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB connection successful!"));
 
 app.listen(port, () => {
   console.log(`Server running on port : ${port}`);
